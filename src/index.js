495d924418f09b448eaf120a241dcfdedb658fe6
M.M.Baqir <rbaqir41@gmail.com>
2025-03-11 16:18:05 +0500

    utils code update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 // require("dotenv").config({path: './env'})
-
 import dotenv from "dotenv";
 import connetDB from "./db/connection.js";
-import path from "path";
+import app from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 // const app = express();
 
-
-connetDB();
-
-
-
-
-
-
-
-
-
+connetDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`ERROR: `, error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection failed: `, err);
+  });
 
 /*
 (async () => {
